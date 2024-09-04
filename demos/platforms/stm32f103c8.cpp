69e3155f7c97f44bbfb74cc0b2c220009e73dc25
Malia Labor <99438964+MaliaLabor@users.noreply.github.com>
2024-09-03 18:30:18 -0700

    :sparkles: Add driver for apa102 LEDs (#6)
    * :sparkles: Add driver for apa102 LEDs
    
    * add changes requested
    
    * add extra comment
    
    * fix clang format
    
    * Rename stuff to pixels instead of leds
    
    * add missing initializer for CI
    
    * one more missing initializer

--- demos/platforms/stm32f103c8.cpp
+++ demos/platforms/stm32f103c8.cpp
@@ -18,16 +18,22 @@
 #include <libhal-armcortex/startup.hpp>
 #include <libhal-armcortex/system_control.hpp>
 
-#include <libhal-stm32f1/clock.hpp>
-#include <libhal-stm32f1/constants.hpp>
-#include <libhal-stm32f1/output_pin.hpp>
-#include <libhal-stm32f1/uart.hpp>
+#include <libhal-arm-mcu/stm32f1/clock.hpp>
+#include <libhal-arm-mcu/stm32f1/constants.hpp>
+#include <libhal-arm-mcu/stm32f1/input_pin.hpp>
+#include <libhal-arm-mcu/stm32f1/output_pin.hpp>
+#include <libhal-arm-mcu/stm32f1/spi.hpp>
+#include <libhal-arm-mcu/stm32f1/uart.hpp>
+#include <libhal-soft/bit_bang_spi.hpp>
+#include <libhal-util/steady_clock.hpp>
 
 #include <resource_list.hpp>
 
 resource_list initialize_platform()
 {
   using namespace hal::literals;
+
+  constexpr bool use_bit_bang_spi = true;
 
   // Set the MCU to the maximum clock speed
   hal::stm32f1::maximum_speed_using_internal_oscillator();
@@ -41,12 +47,39 @@ resource_list initialize_platform()
                                     .baud_rate = 115200,
                                   });
 
+  auto cpu_frequency = hal::stm32f1::frequency(hal::stm32f1::peripheral::cpu);
+  static hal::cortex_m::dwt_counter steady_clock(cpu_frequency);
   static hal::stm32f1::output_pin led('C', 13);
+  static hal::stm32f1::output_pin spi_chip_select('A', 4);
+  spi_chip_select.level(true);
 
-  return {
-    .reset = +[]() { hal::cortex_m::reset(); },
-    .console = &uart1,
-    .clock = &counter,
-    .status_led = &led,
+  hal::spi* spi = nullptr;
+  static hal::spi::settings spi_settings{
+    .clock_rate = 250.0_kHz,
+    .clock_polarity = false,
+    .clock_phase = true,
   };
+
+  if constexpr (use_bit_bang_spi) {
+    static hal::stm32f1::output_pin sck('A', 5);
+    static hal::stm32f1::output_pin copi('A', 6);
+    static hal::stm32f1::input_pin cipo('A', 7);
+    static hal::soft::bit_bang_spi::pins bit_bang_spi_pins{ .sck = &sck,
+                                                            .copi = &copi,
+                                                            .cipo = &cipo };
+
+    static hal::soft::bit_bang_spi bit_bang_spi(
+      bit_bang_spi_pins, steady_clock, spi_settings);
+    spi = &bit_bang_spi;
+  } else {
+    static hal::stm32f1::spi spi1(hal::bus<1>, spi_settings);
+    spi = &spi1;
+  }
+
+  return { .reset = +[]() { hal::cortex_m::reset(); },
+           .console = &uart1,
+           .clock = &counter,
+           .status_led = &led,
+           .spi = spi,
+           .spi_chip_select = &spi_chip_select };
 }
---
+++ src/apa102.cpp
@@ -0,0 +1,26 @@
+#include <libhal-display/apa102.hpp>
+
+#include <libhal-util/as_bytes.hpp>
+#include <libhal-util/spi.hpp>
+#include <span>
+
+namespace hal::display {
+
+apa102::apa102(hal::spi& p_spi, hal::output_pin& p_chip_select)
+  : m_spi(&p_spi)
+  , m_chip_select(&p_chip_select)
+{
+  // 1 MHz is max speed LEDs can handle
+  m_spi->configure(hal::spi::settings{ 1.0_MHz, { false }, { false } });
+}
+
+// public
+void apa102::update(std::span<apa102_pixel> p_pixels)
+{
+  m_chip_select->level(false);
+  hal::write(*m_spi, std::array<hal::byte, 4>{ 0x00, 0x00, 0x00, 0x00 });
+  hal::write(*m_spi, hal::as_bytes(p_pixels));
+  hal::write(*m_spi, std::array<hal::byte, 4>{ 0xFF, 0xFF, 0xFF, 0xFF });
+  m_chip_select->level(true);
+}
+}  // namespace hal::display
